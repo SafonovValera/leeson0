b42a5f9ab2e58b2088aa00312438a9caee2fa8eb
SafonovValera <o33o.post@gmail.com>
2024-10-16 11:48:12 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+course_title = 'Pyton'
+time_for_one_task = number_of_hours_spent / number_of_completed_tasks
+print('Курс: ', str(course_title), ', всего задач: ', str(number_of_completed_tasks), ', затрачено часов: ',
+      str(number_of_hours_spent), ', среднее время выполнения: ', str(time_for_one_task), ' часа.')
