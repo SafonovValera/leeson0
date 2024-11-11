040e55b1e6aedac3418ae54a7195827ca6d7587d
SafonovValera <o33o.post@gmail.com>
2024-11-11 09:31:28 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return 'Eror'
+    else:
+        rest = first / second
+        return rest
---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as dfm
+
+from true_math import divide as dtm
+
+print(dfm(69, 3))
+print(dfm(3, 0))
+print(dtm(49, 7))
+print(dtm(15, 0))
