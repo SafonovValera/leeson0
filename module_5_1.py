3e42e9746c0bc48e80d75a4c5717ee583b969c5f
SafonovValera <o33o.post@gmail.com>
2024-11-23 11:11:49 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -7,6 +7,8 @@ class House:
         if new_floor < self.number_of_floors:
             for floor in range(1, new_floor + 1):
                 print(floor)
+        else:
+            print("Такого этажа не существует")
 
 
 h1 = House('ЖК Горский', 18)
@@ -14,5 +16,3 @@ h2 = House('Домик в деревне', 2)
 
 h1.go_to(5)
 h2.go_to(10)
-
-print('Такого этажа не существует!')
