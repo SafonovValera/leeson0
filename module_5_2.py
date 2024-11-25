26fa7c8dd402cb966f06411386a781015814b8f5
SafonovValera <o33o.post@gmail.com>
2024-11-25 08:02:43 +0300

    Add files via upload

--- module_5_2.py
+++ module_5_2.py
@@ -14,12 +14,16 @@ class House:
         return self.number_of_floor
 
     def __str__(self):
-        return f'Название {self.name}, кол-во этажей: {self.number_of_floor}'
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floor}'
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
+
+# __str__
 print(h1)
 print(h2)
+
+# __len__
 print(len(h1))
 print(len(h2))
